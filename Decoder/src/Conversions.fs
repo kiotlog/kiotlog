2fba50ed279c0f8cd8f47e5bd23235d22a1381a1
Giampaolo Mancini <giampaolo@trampolineup.com>
2017-11-18 01:13:42 +0100

    Add Module for conversions functions.

---
+++ Decoder/src/Conversions.fs
@@ -0,0 +1,19 @@
+namespace Decoder
+
+module KlConversions =
+
+    let int16ToFloat (value : float) (max : int)  (min : int) : float =
+        let conversionFactor = float (1 <<< 15 - 1) / float (max - min)
+        
+        float value / conversionFactor
+
+    let uint16ToFloat (value : float) (max : int)  (min : int) : float =
+        let conversionFactor = float (1 <<< 16 - 1) / float (max - min)
+        
+        float value / conversionFactor
+
+    let inline klConvert value max min fn =
+        match fn with
+        | "float_to_int16" -> int16ToFloat value max min
+        | "float_to_uint16" -> uint16ToFloat value max min
+        | _ -> float value
