66ab612abf5ae5527f1600325c8408cae54f2ebe
Giampaolo Mancini <giampaolo@trampolineup.com>
2017-11-18 01:15:36 +0100

    Add decode function!

--- Decoder/src/Decoder.fs
+++ Decoder/src/Decoder.fs
@@ -1,6 +1,43 @@
 namespace Decoder
 
+open System
+
+open Catalog
+open Helpers
+open Body
+open Struct
+open PackedValue
+open KiotlogDB
+open KlConversions
+
 module Decoder =
 
-    let decode =
-        ()
+    let private doConvert (field : PackedValue, sensor : Sensors) =
+        let max = sensor.SensorType.Meta.Max
+        let min = sensor.SensorType.Meta.Min
+        let fn = sensor.Conversion.Fun
+
+        klConvert (field.ToFloat()) max min fn
+
+    let private strToByteArray topic = 
+        match topic with
+        | "sigfox" -> byteArrayFromHexString
+        | "lorawan" -> Convert.FromBase64String
+        | _ -> byteArrayFromHexString
+
+    let klDecode (cs : string) (channel, _, _) (msg : KlBody) =
+        let formatString = getFormatString cs msg.DevId
+        let sortedSensors = getSortedSensors cs msg.DevId |> Seq.toList
+
+        let payload =
+            msg.PayloadRaw
+            |> strToByteArray channel
+            |> unpack formatString
+        
+        // Replace with Dictionary<string, float>
+        let decoded =
+            List.zip payload sortedSensors
+            |> List.map (fun (p, s) -> s.Meta.Name, doConvert (p, s))
+
+        printfn "%A" payload
+        printfn "%A" decoded
