1d59412ab07a08ca8bf074bf6b530f7d208c4f0c
Giampaolo Mancini <giampaolo@trampolineup.com>
2017-11-19 17:23:41 +0100

    Fix operations precedence.

--- Decoder/src/Conversions.fs
+++ Decoder/src/Conversions.fs
@@ -1,14 +1,14 @@
 namespace Decoder
 
-module KlConversions =
+module Conversions =
 
     let int16ToFloat (value : float) (max : int)  (min : int) : float =
-        let conversionFactor = float (1 <<< 15 - 1) / float (max - min)
+        let conversionFactor = float ((1 <<< 15) - 1) / float (max - min)
         
         float value / conversionFactor
 
     let uint16ToFloat (value : float) (max : int)  (min : int) : float =
-        let conversionFactor = float (1 <<< 16 - 1) / float (max - min)
+        let conversionFactor = float ((1 <<< 16) - 1) / float (max - min)
         
         float value / conversionFactor
 
