3bdd7ec452ecdc4390f01eb954850b0b4fee5611
Giampaolo Mancini <giampaolo@trampolineup.com>
2017-11-21 22:55:26 +0100

    Reformat.

--- Decoder/src/Mqtt.fs
+++ Decoder/src/Mqtt.fs
@@ -64,25 +64,23 @@ module Mqtt =
         printfn "Sub Message Subscribed: %A" e.GrantedQoSLevels
 
     let rec mqttConnect (broker : string, port) : MqttClient =
-        let connectClient (cli : MqttClient) =
-            cli.Connect(Guid.NewGuid().ToString())
+        let client = MqttClient (broker, port, false, null, null, MqttSslProtocols.None)
+
+        try
+            client.Connect (Guid.NewGuid().ToString())
             |> function
             | MqttMsgConnack.CONN_REFUSED_PROT_VERS -> failwith "Invalid Protocol Version"
             | MqttMsgConnack.CONN_REFUSED_IDENT_REJECTED -> failwith "Identity Rejected"
             | MqttMsgConnack.CONN_REFUSED_SERVER_UNAVAILABLE -> failwith "Server Unavailable"
             | MqttMsgConnack.CONN_REFUSED_USERNAME_PASSWORD -> failwith "Invalid Username or Password"
             | MqttMsgConnack.CONN_REFUSED_NOT_AUTHORIZED -> failwith "Client Not Authorized"
-            | MqttMsgConnack.CONN_ACCEPTED -> cli
+            | MqttMsgConnack.CONN_ACCEPTED -> client
             | _ -> failwith "Unable to connect: Unknown Connack Type"
-
-        let client = MqttClient(broker, port, false, null, null, MqttSslProtocols.None)
-
-        try
-            connectClient client
         with
-        | :? MqttConnectionException ->
-            Thread.Sleep 10
-            mqttConnect (broker, port)
+            | :? MqttConnectionException ->
+                eprintfn "Connection failed. Retrying in 10 seconds."
+                Thread.Sleep 10
+                mqttConnect (broker, port)
 
     // let connectionClosedHandler (client: MqttClient) parameters (e: EventArgs) =
     //     eprintfn "Broker connection closed: trying to reconnect [%A]" e
