5778906dc5f9129fea17e62735cf3217e6ec0af5
Giampaolo Mancini <giampaolo@trampolineup.com>
2017-11-24 12:53:56 +0100

    Move Decoder to ROP.

--- Decoder/src/Decoder.fs
+++ Decoder/src/Decoder.fs
@@ -18,6 +18,18 @@ open Request
 
 module Decoder =
 
+    type Decoding = {
+        Ctx : Context
+
+        Device : Devices option
+        Sensors : Sensors list option
+        FmtString : string option
+
+        Unpacked : PackedValue list option
+
+        Data : Dictionary<string, float> option
+    }
+
     let private strToByteArray channel = 
         match channel with
         | "sigfox" -> byteArrayFromHexString
@@ -43,48 +55,62 @@ module Decoder =
 
             ok decodedDict
 
-    let private decodePayload channel rawPayload device =
-        let sortedSensors =
-            getSortedSensors device
-            |> returnOrFail
-            |> Seq.toList
-
-        let formatString =
-            getFormatString device
-            |> returnOrFail
-        
-        let payload =
-            rawPayload
-            |> strToByteArray channel
-
-        let validatedUnpack p =
-            unpack formatString p
-            |> bind (convertMeasures sortedSensors)
-        
-        payload
-        |> validatedUnpack
+    let private serializeData dtx =
+        JsonConvert.SerializeObject(dtx.Data.Value, Formatting.None)
     
+    let private getDevice devices dtx =
+        let _, _, device = dtx.Ctx.TopicParts.Value
+        match getDevice device devices with
+        | Ok (x, _) -> ok { dtx with Device = Some x }
+        | Bad msgs -> fail msgs
+
+    let private getSortedSensors dtx =
+        match getSortedSensors dtx.Device.Value with
+        | Ok (x, _) -> ok { dtx with Sensors = Some (x |> Seq.toList) }
+        | Bad msgs -> fail msgs
+
+    let private getFormatString dtx =
+        match getFormatString dtx.Device.Value with
+        | Ok (x, _) -> ok { dtx with FmtString = Some x }
+        | Bad msgs -> fail msgs
+    
+    let private unpackStruct dtx =
+        let channel, _, device = dtx.Ctx.TopicParts.Value
+        let payload =
+            dtx.Ctx.PayloadRaw.Value
+            |> strToByteArray channel
+        match unpack dtx.FmtString.Value payload with
+        | Ok (x, _) -> ok { dtx with Unpacked = Some x }
+        | Bad msgs -> fail (sprintf "[%s]" device::msgs)
+
+    let private convertFields dtx =
+        match convertMeasures dtx.Sensors.Value dtx.Unpacked.Value with
+        | Ok (x, _) -> ok { dtx with Data = Some x }
+        | Bad msgs -> fail msgs
+
     let klDecode (cs : string) ctx =
 
-        let channel, _, device = ctx.TopicParts.Value
+        let decoding =
+            {
+                Ctx = ctx
+                Device = None
+                Sensors = None
+                FmtString = None
+                Unpacked = None
+                Data = None
+            }
 
         use dbCtx = new KiotlogDBContext(cs)
         let devices = getDevices dbCtx
 
-        let serializeData d =
-            ok (JsonConvert.SerializeObject(d, Formatting.None))
-
-        let validatedDecode p =        
-            getDevice device devices
-            |> bind (decodePayload channel p)
+        let decode =
+            getDevice devices
+            >> bind getSortedSensors
+            >> bind getFormatString
+            >> bind unpackStruct
+            >> bind convertFields
+            >> lift serializeData
         
-        let data =
-            let success (x, _) = x
-            let failure msgs = eprintfn "%A" msgs; "{}"
-
-            ctx.PayloadRaw.Value
-            |> validatedDecode
-            |> bind serializeData
-            |> either success failure
-        
-        ok { ctx with Data = Some data }
+        match decode decoding with
+        | Ok (x, _) -> ok { ctx with Data = Some x }
+        | Bad msgs -> fail (sprintf "Decoding failed: %A" msgs)
