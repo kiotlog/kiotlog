df9d971ef76498c2d8d627fd4c4eaf7b4674e6ed
Giampaolo Mancini <giampaolo@trampolineup.com>
2018-01-10 15:35:02 +0100

    Add GPLv3 license and notice.

--- SensorTypes.cs
+++ SensorTypes.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+    Copyright (C) 2017 Giampaolo Mancini, Trampoline SRL.
+    Copyright (C) 2017 Francesco Varano, Trampoline SRL.
+
+    This file is part of Kiotlog.
+
+    Kiotlog is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kiotlog is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -36,7 +56,7 @@ namespace KiotlogDB
             get { return _Meta == null ? null : JsonConvert.DeserializeObject<JsonBMeta>(_Meta, JsonSettings.snakeSettings); }
             set { _Meta = value == null ? null : JsonConvert.SerializeObject(value, JsonSettings.snakeSettings); }
         }
-        
+
         [Required]
         [Column("type")]
         public string Type { get; set; }
