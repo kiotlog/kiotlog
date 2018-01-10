92c69256f7767d649b64b3bd44fbc760fee21b56
Giampaolo Mancini <giampaolo@trampolineup.com>
2018-01-10 15:35:02 +0100

    Add GPLv3 license and notice.

--- Decoder/src/Helpers.fs
+++ Decoder/src/Helpers.fs
@@ -1,3 +1,23 @@
+(*
+    Copyright (C) 2017 Giampaolo Mancini, Trampoline SRL.
+    Copyright (C) 2017 Francesco Varano, Trampoline SRL.
+
+    This file is part of Kiotlog.
+
+    Kiotlog is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kiotlog is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 namespace Decoder
 
 open System.Text
@@ -11,21 +31,21 @@ module Helpers =
     let encode (str: string) =
         str |> Encoding.ASCII.GetBytes
 
-    let byteArrayFromHexString (hex: string) = 
+    let byteArrayFromHexString (hex: string) =
         let joinTwoChars = Seq.map (string) >> Seq.reduce (+)
         let hexCharsToByte chs = Convert.ToByte (chs, 16)
         let twoCharsToByte = joinTwoChars >> hexCharsToByte
-        
+
         hex
         |> Seq.chunkBySize 2
         |> Seq.map twoCharsToByte
         |> Seq.toArray
-    
+
     let hexStringFromByteArray (bytes : byte []) =
         bytes
         |> Array.map (fun b -> sprintf "%02x" b)
         |> Array.reduce (+)
-    
+
     let unixTimeStampToDateTime ts =
          let dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds ts
          dateTimeOffset.UtcDateTime
--- KlsnReceiver/src/Program.fs
+++ KlsnReceiver/src/Program.fs
@@ -3,6 +3,26 @@
 open System
 open System.Threading
 
+(*
+    Copyright (C) 2017 Giampaolo Mancini, Trampoline SRL.
+    Copyright (C) 2017 Francesco Varano, Trampoline SRL.
+
+    This file is part of Kiotlog.
+
+    Kiotlog is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kiotlog is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
 open uPLibrary.Networking.M2Mqtt.Messages
 
 open Arguments
@@ -22,10 +42,10 @@ module Program =
             [| for _ in mainConfig.Topics -> MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE |]
 
         let mqttClientId = "KlsnReceiver/" + Guid.NewGuid().ToString()
-        
+
         let mqttClient =
             mqttConnect mainConfig.MQTTBroker mqttClientId
-        
+
         let mqttClosed =
             mqttClosed mqttClient mqttClientId
 
