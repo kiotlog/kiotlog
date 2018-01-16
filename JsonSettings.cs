4bf7250e297a8fc0a89b0a9f26f8288e90997126
Giampaolo Mancini <giampaolo@trampolineup.com>
2018-01-16 15:25:36 +0100

    Squashed 'KiotlogDB/' content from commit df3957e
    git-subtree-dir: KiotlogDB
    git-subtree-split: df3957e526184c5531da7e64c4bce745df412678

---
+++ JsonSettings.cs
@@ -0,0 +1,36 @@
+/*
+    Copyright (C) 2017 Giampaolo Mancini, Trampoline SRL.
+    Copyright (C) 2017 Francesco Varano, Trampoline SRL.
+
+    This file is part of Kiotlog.
+
+    Kiotlog is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Kiotlog is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace KiotlogDB
+{
+    class JsonSettings {
+        static public JsonSerializerSettings snakeSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Error,
+            ContractResolver = new DefaultContractResolver {
+                NamingStrategy = new SnakeCaseNamingStrategy(true, false, false)
+            }
+        };
+    }
+}
