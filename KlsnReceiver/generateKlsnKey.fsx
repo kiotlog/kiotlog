e768ae153c891e0b31e749488a7f22a87d0245fc
Giampaolo Mancini <giampaolo@trampolineup.com>
2018-01-25 18:04:25 +0100

    Print key in C include style

--- KlsnReceiver/generateKlsnKey.fsx
+++ KlsnReceiver/generateKlsnKey.fsx
@@ -6,4 +6,13 @@ let mutable randomNumber : byte [] = Array.zeroCreate 32
 rngCsp.GetBytes(randomNumber)
 
 let klsnKey = randomNumber |> Convert.ToBase64String
-printfn "%s" klsnKey
+printfn "Base64:\n  %s" klsnKey
+
+let randomNumberCInclude =
+    randomNumber
+    |> Array.map (sprintf "0x%02x")
+    |> Array.chunkBySize 8
+    |> Array.map (String.concat ", " >> sprintf "  %s")
+    |> String.concat "\n"
+
+printfn "C include:\n%s" randomNumberCInclude
