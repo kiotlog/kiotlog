ff562965656fe665c0da69c8a483611158e5a31f
Giampaolo Mancini <mancho@honkytonky.local>
2018-03-07 16:55:10 +0100

    Squashed 'KiotlogDBF/' changes from 86b565e..d524645
    d524645 Move a lot of stuff to DbContext
    6effb9c Cosmetics
    adf00e6 Move to EF Core 2.1 (preview) and start using Value Conversions.
    
    git-subtree-dir: KiotlogDBF
    git-subtree-split: d5246459b4a9320b05c52e07617a8f75ab81e0a8

--- KiotlogDBFContext.fs
+++ KiotlogDBFContext.fs
@@ -20,12 +20,13 @@
 
 namespace KiotlogDBF.Context
 
-open KiotlogDBF.Models
-open Microsoft.EntityFrameworkCore
 open System.Collections.Generic
+open Microsoft.EntityFrameworkCore
+
+open KiotlogDBF.Models
+open KiotlogDBF.Json
 
 // https://stackoverflow.com/questions/5423768/c-sharp-to-f-ef-code-first
-
 type KiotlogDBFContext (dbContextOptions: DbContextOptions<KiotlogDBFContext>) =
     inherit DbContext (dbContextOptions)
 
@@ -64,23 +65,58 @@ type KiotlogDBFContext (dbContextOptions: DbContextOptions<KiotlogDBFContext>) =
 
         modelBuilder.Entity<Devices>(
             fun entity ->
-                entity.HasKey(fun d -> d.Id :> obj).HasName("devices_pkey") |> ignore
-                entity.HasIndex(fun d -> d.Device :> obj).HasName("devices_device_key").IsUnique |> ignore
-                entity.Property(fun d -> d.Id).HasDefaultValueSql("gen_random_uuid()") |> ignore
-                entity.Property(fun d -> d._Meta).HasDefaultValueSql("'{}'::jsonb") |> ignore
-                entity.Property(fun d -> d._Auth).HasDefaultValueSql("json_build_object('klsn', json_build_object('key', encode(gen_random_bytes(32), 'base64')), 'basic', json_build_object('token', encode(gen_random_bytes(32), 'base64')))") |> ignore
-                entity.Property(fun d -> d._Frame).HasDefaultValueSql("'{\"bigendian\": true, \"bitfields\": false}'::jsonb") |> ignore
+                entity.ToTable("devices") |> ignore
+                entity.HasKey(fun d -> d.Id :> obj)
+                    .HasName("devices_pkey") |> ignore
+                entity.HasIndex(fun d -> d.Device :> obj)
+                    .HasName("devices_device_key").IsUnique |> ignore
+                entity.Property(fun d -> d.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("gen_random_uuid()") |> ignore
+                entity.Property(fun d -> d.Device)
+                    .HasColumnName("device")
+                    .IsRequired()
+                    .HasDefaultValueSql("device") |> ignore
+                entity.Property(fun d -> d.Meta)
+                    .HasColumnName("meta")
+                    .HasColumnType("jsonb")
+                    .HasConversion(jsonConverter<DevicesMeta>)
+                    .HasDefaultValueSql("'{}'::jsonb") |> ignore
+                entity.Property(fun d -> d.Auth)
+                    .HasColumnName("auth")
+                    .HasColumnType("jsonb")
+                    .HasConversion(jsonConverter<DevicesAuth>)
+                    .HasDefaultValueSql("json_build_object('klsn', json_build_object('key', encode(gen_random_bytes(32), 'base64')), 'basic', json_build_object('token', encode(gen_random_bytes(32), 'base64')))") |> ignore
+                entity.Property(fun d -> d.Frame)
+                    .HasColumnName("frame")
+                    .HasColumnType("jsonb")
+                    .HasConversion(jsonConverter<DevicesFrame>)
+                    .HasDefaultValueSql(""" '{"bigendian": true, "bitfields": false}'::jsonb """) |> ignore
         ) |> ignore
 
         modelBuilder.Entity<Points>(
             fun entity ->
+                entity.ToTable("points") |> ignore
                 entity.HasKey(fun p -> p.Id :> obj).HasName("points_pkey") |> ignore
-                entity.Property(fun p -> p.Id).HasDefaultValueSql("gen_random_uuid()") |> ignore
-                entity.Property(fun p -> p.Data).HasDefaultValueSql("'{}'::jsonb") |> ignore
-                entity.Property(fun p -> p.Flags).HasDefaultValueSql("'{}'::jsonb") |> ignore
-                entity.Property(fun p -> p.Time).HasDefaultValueSql("now()") |> ignore
+                entity.Property(fun p -> p.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("gen_random_uuid()") |> ignore
+                entity.Property(fun p -> p.Data)
+                    .HasColumnName("data")
+                    .HasColumnType("jsonb")
+                    .IsRequired()
+                    .HasDefaultValueSql("'{}'::jsonb") |> ignore
+                entity.Property(fun p -> p.Flags)
+                    .HasColumnName("flags")
+                    .HasColumnType("jsonb")
+                    .IsRequired()
+                    .HasDefaultValueSql("'{}'::jsonb") |> ignore
+                entity.Property(fun p -> p.Time)
+                    .HasColumnName("time")                
+                    .HasDefaultValueSql("now()") |> ignore
+                entity.Property("DeviceId").HasColumnName("device_id") |> ignore                
                 entity.HasOne(fun p -> p.Device)
-                    .WithMany(fun (d : Devices) -> d.Points :> IEnumerable<Points>)
+                    .WithMany(fun (d : Devices) -> d.Points :> IEnumerable<_>)
                     .HasForeignKey(fun (p : Points) -> p.DeviceId :> obj)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("points_device_fkey")
@@ -89,37 +125,72 @@ type KiotlogDBFContext (dbContextOptions: DbContextOptions<KiotlogDBFContext>) =
 
         modelBuilder.Entity<Sensors>(
             fun entity ->
+                entity.ToTable("sensors") |> ignore
                 entity.HasKey(fun s -> s.Id :> obj).HasName("sensors_pkey") |> ignore
-                entity.Property(fun s -> s.Id).HasDefaultValueSql("gen_random_uuid()")  |> ignore
-                entity.Property(fun s -> s._Fmt).HasDefaultValueSql("'{}'::jsonb")  |> ignore
-                entity.Property(fun s -> s._Meta).HasDefaultValueSql("'{}'::jsonb")  |> ignore
+                entity.Property(fun s -> s.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("gen_random_uuid()")  |> ignore
+                entity.Property(fun s -> s.Fmt)
+                    .HasColumnName("fmt")
+                    .HasColumnType("jsonb")                    
+                    .HasConversion(jsonConverter<SensorsFmt>)
+                    .HasDefaultValueSql("'{}'::jsonb")  |> ignore
+                entity.Property(fun s -> s.Meta)
+                    .HasColumnName("meta")
+                    .HasColumnType("jsonb")
+                    .HasConversion(jsonConverter<SensorsMeta>)                    
+                    .HasDefaultValueSql("'{}'::jsonb")  |> ignore
+                entity.Property("DeviceId")
+                    .HasColumnName("device_id")
+                    .HasColumnType("uuid") |> ignore                
                 entity.HasOne(fun s -> s.Device)
-                    .WithMany(fun (d : Devices) -> d.Sensors :> IEnumerable<Sensors>)
+                    .WithMany(fun (d : Devices) -> d.Sensors :> IEnumerable<_>)
                     .HasForeignKey(fun (s : Sensors) -> s.DeviceId :> obj)
                     .HasConstraintName("sensors_device_id_fkey") |> ignore
+                entity.Property("SensorTypeId").HasColumnName("sensor_type_id") |> ignore                
                 entity.HasOne(fun s -> s.SensorType)
-                    .WithMany(fun (t : SensorTypes) -> t.Sensors :> IEnumerable<Sensors>)
+                    .WithMany(fun (t : SensorTypes) -> t.Sensors :> IEnumerable<_>)
                     .HasForeignKey(fun (s: Sensors) -> s.SensorTypeId :> obj)
                     .HasConstraintName("sensors_sensor_type_fkey")  |> ignore
+                entity.Property("ConversionId").HasColumnName("conversion_id") |> ignore                
                 entity.HasOne(fun s -> s.Conversion)
-                    .WithMany(fun (c : Conversions) -> c.Sensors :> IEnumerable<Sensors>)
+                    .WithMany(fun (c : Conversions) -> c.Sensors :> IEnumerable<_>)
                     .HasForeignKey(fun (s : Sensors) -> s.ConversionId :> obj)
                     .HasConstraintName("sensors_conversion_fkey") |> ignore
             ) |> ignore
 
         modelBuilder.Entity<SensorTypes>(
             fun entity ->
+                entity.ToTable("sensor_types") |> ignore
                 entity.HasKey(fun t -> t.Id :> obj).HasName("sensor_types_pkey") |> ignore
                 entity.HasIndex(fun t -> t.Name :> obj).HasName("sensor_types_name_key").IsUnique()  |> ignore
-                entity.Property(fun t -> t.Id).HasDefaultValueSql("gen_random_uuid()")  |> ignore
-                entity.Property(fun t -> t._Meta).HasDefaultValueSql("'{}'::jsonb")  |> ignore
-                entity.Property(fun t -> t.Name).HasDefaultValueSql("'generic'::text") |> ignore
-                entity.Property(fun t -> t.Kind).HasDefaultValueSql("'generic'::text") |> ignore
+                entity.Property(fun t -> t.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("gen_random_uuid()")  |> ignore
+                entity.Property(fun t -> t.Meta)
+                    .HasColumnName("meta")
+                    .HasColumnType("jsonb")
+                    .HasConversion(jsonConverter<SensorTypesMeta>)                
+                    .HasDefaultValueSql("'{}'::jsonb")  |> ignore
+                entity.Property(fun t -> t.Name)
+                    .HasColumnName("name")
+                    .IsRequired()                    
+                    .HasDefaultValueSql("'generic'::text") |> ignore
+                entity.Property(fun t -> t.Kind)
+                    .HasColumnName("kind")
+                    .IsRequired()                    
+                    .HasDefaultValueSql("'generic'::text") |> ignore
         ) |> ignore
 
         modelBuilder.Entity<Conversions>(
             fun entity ->
+                entity.ToTable("conversions") |> ignore
                 entity.HasKey(fun c -> c.Id :> obj).HasName("convertions_pkey") |> ignore
-                entity.Property(fun c -> c.Id).HasDefaultValueSql("gen_random_uuid()")  |> ignore
-                entity.Property(fun c -> c.Fun).HasDefaultValueSql("'id'::text") |> ignore
+                entity.Property(fun c -> c.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("gen_random_uuid()")  |> ignore
+                entity.Property(fun c -> c.Fun)
+                    .HasColumnName("fun")
+                    .IsRequired()
+                    .HasDefaultValueSql("'id'::text") |> ignore
         ) |> ignore
--- Models.fs
+++ Models.fs
@@ -22,225 +22,79 @@ namespace KiotlogDBF.Models
 
 open System
 open System.Collections.Generic
-open System.ComponentModel.DataAnnotations
-open System.ComponentModel.DataAnnotations.Schema
-open Newtonsoft.Json
 
 open KiotlogDBF.Json
 
 [<AllowNullLiteral>]
-[<NotMapped>]
-type DevicesMeta () =
-    member val Name = null with get, set
-    member val Description = null with get, set
-    member val Kind = null with get, set
-
-[<AllowNullLiteral>]
-[<NotMapped>]
-type DevicesAuth () =
-    member val Basic : BasicAuth = null with get, set
-    member val Klsn : KlsnAuth = null with get, set
-and [<AllowNullLiteral>]
-    [<NotMapped>]
-    BasicAuth () =
-    member val Token : string = null with get, set
-and [<AllowNullLiteral>]
-    [<NotMapped>]
-    KlsnAuth () =
-    member val Key : string = null with get, set
-
-[<AllowNullLiteral>]
-[<NotMapped>]
-type DevicesFrame () =
-    member val Bigendian = false with get, set
-    member val Bitfields = false with get, set
-
-[<NotMapped>]
-type SensorsFmt = {
-    Index: int
-    FmtChr: string
-}
-
-[<NotMapped>]
-type SensorsMeta = {
-    Name: string
-    Description: string
-}
-
-[<NotMapped>]
-type SensorTypesMeta = {
-    Max: int
-    Min: int
-}
-
-[<AllowNullLiteral>]
-[<Table("devices")>]
 type Devices () =
 
-    [<Column("meta", TypeName = "jsonb")>]
-    member val internal _Meta = null with get, set
-
-    [<Column("auth", TypeName = "jsonb")>]
-    member val internal _Auth = null with get, set
-
-    [<Column("frame", TypeName = "jsonb")>]
-    member val internal _Frame = null with get, set
-
-    [<Column("id")>]
     member val Id = Guid.Empty with get, set
-
-    [<Column("device")>]
-    [<Required(ErrorMessage="The Device field is Required")>]
     member val Device = String.Empty with get, set
+    member val Meta : DevicesMeta = null with get, set
+    member val Auth : DevicesAuth = null with get, set
+    member val Frame : DevicesFrame = null with get, set
 
-    [<NotMapped>]
-    member public this.Meta
-        with get () = jsonGetter<DevicesMeta> this._Meta
-        and set (value: DevicesMeta) = this._Meta <- jsonSetter<DevicesMeta> value
-
-    [<NotMapped>]
-    member this.Auth
-        with get () = jsonGetter<DevicesAuth> this._Auth
-        and set value = this._Auth <- jsonSetter (value: DevicesAuth)
-
-    [<NotMapped>]
-    member this.Frame
-        with get () = jsonGetter<DevicesFrame> this._Frame
-        and set value = this._Frame <- jsonSetter<DevicesFrame> value
-
-    [<InverseProperty("Device")>]
-    member val Points = HashSet<Points>() :> ICollection<_> with get, set
-
-    [<InverseProperty("Device")>]
-    member val Sensors = HashSet<Sensors>() :> ICollection<_> with get, set
+    member val Points = HashSet<Points>() with get, set
+    member val Sensors = HashSet<Sensors>() with get, set
 
     override this.ToString() =
         toJsonString this
 
-    static member public ShouldSerialize_Meta () = false
-    static member public ShouldSerialize_Auth () = false
-    static member public ShouldSerialize_Frame () = false
     static member public ShouldSerializePoints () = false
+    member public this.ShouldSerializeSensors () = not (Seq.isEmpty this.Sensors)
 
 and [<AllowNullLiteral>]
-    [<Table("points")>]
     Points () =
 
-    [<Column("id")>]
     member val Id = Guid.Empty with get, set
-
-    [<Column("device_id")>]
     member val DeviceId = Guid.Empty with get, set
-
-    [<Column("time", TypeName = "timestamptz")>]
     member val Time = DateTime.Now with get, set
-
-    [<Required>]
-    [<Column("flags", TypeName = "jsonb")>]
     member val Flags = "{}" with get, set
-
-    [<Required>]
-    [<Column("data", TypeName = "jsonb")>]
     member val Data = "{}" with get, set
 
-    [<ForeignKey("DeviceId")>]
-    [<InverseProperty("Points")>]
     member val Device : Devices = null with get, set
 
     override this.ToString() =
         toJsonString this
 
 and [<AllowNullLiteral>]
-    [<Table("sensors")>]
     Sensors() =
 
-    [<Column("meta", TypeName = "jsonb")>]
-    member val internal _Meta = String.Empty with get, set
-
-    [<Column("fmt", TypeName = "jsonb")>]
-    member val internal _Fmt = String.Empty with get, set
-
-    [<Column("id")>]
     member val Id = Guid.Empty with get, set
-
-    [<NotMapped>]
-    member public this.Meta
-        with get () = JsonConvert.DeserializeObject<SensorsMeta>(this._Meta, snakeSettings)
-        and  set (value: SensorsMeta) = this._Meta <- JsonConvert.SerializeObject(value, snakeSettings)
-
-    [<NotMapped>]
-    member public this.Fmt
-        with get () = JsonConvert.DeserializeObject<SensorsFmt>(this._Fmt, snakeSettings)
-        and  set (value: SensorsFmt) = this._Fmt <- JsonConvert.SerializeObject(value, snakeSettings)
-
-    [<Column("device_id")>]
+    member val Meta : SensorsMeta = null with get, set
+    member val Fmt : SensorsFmt = null with get, set
     member val DeviceId = Guid.Empty with get, set
-
-    [<Column("sensor_type_id")>]
     member val SensorTypeId = Guid.Empty with get, set
-
-    [<Column("conversion_id")>]
     member val ConversionId = Guid.Empty with get, set
 
-    [<ForeignKey("DeviceId")>]
-    [<InverseProperty("Sensors")>]
     member val Device : Devices = null with get, set
-
-    [<ForeignKey("SensorTypeId")>]
-    [<InverseProperty("Sensors")>]
     member val SensorType : SensorTypes = null with get, set
-
-    [<ForeignKey("ConversionId")>]
-    [<InverseProperty("Sensors")>]
     member val Conversion : Conversions = null with get, set
 
     override this.ToString() =
         toJsonString this
 
 and [<AllowNullLiteral>]
-    [<Table("sensor_types")>]
     SensorTypes() =
 
-    [<Column("meta", TypeName = "jsonb")>]
-    member val _Meta = String.Empty with get, set
-
-    [<Column("id")>]
+    member val Meta : SensorTypesMeta = null with get, set
     member val Id = Guid.Empty with get, set
-
-    [<Required>]
-    [<Column("name")>]
     member val Name = String.Empty with get, set
-
-    [<Required>]
-    [<Column("kind")>]
     member val Kind = String.Empty with get, set
 
-    [<NotMapped>]
-    member public this.Meta
-        with get () = JsonConvert.DeserializeObject<SensorTypesMeta>(this._Meta)
-        and  set(value: SensorTypesMeta) = this._Meta <- JsonConvert.SerializeObject(value)
+    member val Sensors = HashSet<Sensors>() with get, set
 
-    [<InverseProperty("SensorType")>]
-    member val Sensors = HashSet<Sensors>() :> ICollection<_> with get, set
-
-    static member public ShouldSerialize_Meta () = false
     static member public ShouldSerializeSensors () = false
 
     override this.ToString() =
         toJsonString this
 
 and [<AllowNullLiteral>]
-    [<Table("conversions")>]
     Conversions() =
 
-    [<Column("id")>]
     member val Id = Guid.Empty with get, set
-
-    [<Required>]
-    [<Column("fun")>]
     member val Fun = String.Empty with get, set
 
-    [<InverseProperty("Conversion")>]
-    member val Sensors = HashSet<Sensors>() :> ICollection<_> with get, set
+    member val Sensors = HashSet<Sensors>() with get, set
 
     static member public ShouldSerializeSensors () = false
