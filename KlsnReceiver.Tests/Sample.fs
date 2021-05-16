31e35d39bd9033855a9c8be82791d9c8352ef0e1
Giampaolo Mancini <giampaolo@trampolineup.com>
2021-05-16 22:10:57 +0200

    Update MessagePack tests

--- KlsnReceiver.Tests/Sample.fs
+++ KlsnReceiver.Tests/Sample.fs
@@ -3,14 +3,18 @@ module Tests
 open KlsnReceiver.SnPacket
 
 open Expecto
+
 open MessagePack
 open MessagePack.Resolvers
 open MessagePack.FSharp
 
-CompositeResolver.RegisterAndSetAsDefault(
-    FSharpResolver.Instance,
-    StandardResolver.Instance
-)
+let private options =
+    let resolver =
+        Resolvers.CompositeResolver.Create(
+            FSharpResolver.Instance,
+            StandardResolver.Instance
+        )
+    MessagePackSerializerOptions.Standard.WithResolver(resolver)
 
 [<Tests>]
 let tests =
@@ -20,7 +24,8 @@ let tests =
                 { Nonce = "Hello, World!" |> System.Text.Encoding.UTF8.GetBytes
                   Data =  [| 0uy; 1uy; 2uy; 3uy; 4uy; 5uy; 6uy; 7uy; 8uy; 9uy |] }
 
-            let msg = MessagePackSerializer.Serialize(packet)
+            let msg = MessagePackSerializer.Serialize(packet, options)
+
             let parsedMsg = parseSnPacket<SnPacket> msg
             Expect.equal parsedMsg packet "parsedMsg is msg"
 
@@ -30,7 +35,8 @@ let tests =
                   Data =  [| 0uy; 1uy; 2uy; 3uy; 4uy; 5uy; 6uy; 7uy; 8uy; 9uy |]
                   Timestamp = System.DateTimeOffset.UtcNow.ToUnixTimeSeconds() |> System.BitConverter.GetBytes  }
 
-            let msg = MessagePackSerializer.Serialize(packetTs)
+            let msg = MessagePackSerializer.Serialize(packetTs, options)
+
             let parsedMsg = parseSnPacket<SnPacketTs> msg
             Expect.equal parsedMsg packetTs "parsedMsg is msg"
     ]
